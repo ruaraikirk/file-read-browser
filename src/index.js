a62664db9395ae9e01e57218696541731b52ea40
Ruarai Kirk <ruaraikirk@gmail.com>
2019-03-26 16:00:32 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ImportFromFileBodyComponent = () => {
+    let fileReader;
+
+    const handleFileRead = (e) => {
+        const content = fileReader.result;
+        console.log(content);
+        // … do something with the 'content' …
+    };
+
+    const handleFileChosen = (file) => {
+        fileReader = new FileReader();
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsBinaryString(file);
+    };
+
+    return <div className='upload-expense'>
+        <input type='file'
+               id='file'
+               className='input-file'
+               accept='.sketch'
+               onChange={e => handleFileChosen(e.target.files[0])}
+        />
+    </div>;
+};
+
+ReactDOM.render(<ImportFromFileBodyComponent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
